c971ec30bc89dd363e062b3306e55ab2c326c938
sltung98 <tungsiuleung@gmail.com>
2024-02-24 18:07:55 +0800

    Refactor HttpSecurity configuration using lambda expressions

--- Account Service (Java)/task/src/account/security/SecurityConfig.java
+++ Account Service (Java)/task/src/account/security/SecurityConfig.java
@@ -6,9 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -24,6 +27,7 @@ public class SecurityConfig {
     BCryptPasswordEncoder passwordEncoder;
     UserService userDetailsService;
     private CustomAuthenticationEntryPoint authenticationEntryPoint;
+
     @Autowired
     public SecurityConfig(BCryptPasswordEncoder passwordEncoder, UserService userDetailsService, CustomAuthenticationEntryPoint authenticationEntryPoint) {
         this.passwordEncoder = passwordEncoder;
@@ -33,25 +37,27 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests()
-                .requestMatchers(toH2Console())
-                .permitAll()
-                .requestMatchers("/api/auth/signup", "/actuator/shutdown",
-                        "/error")
-                .permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic()
-                .authenticationEntryPoint(authenticationEntryPoint)
-                .and()
-                .csrf().disable()
-                .headers().frameOptions().disable() // for Postman, the H2 console
-                .and()
-                // other matchers
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS); // no session
-        return http.build();
+        return http
+                .authorizeHttpRequests(
+                        authorizeHttpRequestsCustomizer -> authorizeHttpRequestsCustomizer
+                                .requestMatchers(toH2Console()).permitAll()
+                                .requestMatchers("/api/auth/signup", "/actuator/shutdown",
+                                        "/error").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .exceptionHandling(
+                        exceptionHandlingConfigurer -> exceptionHandlingConfigurer
+                                .authenticationEntryPoint(authenticationEntryPoint)
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers((headers) ->
+                        headers
+                                .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+                )
+                .sessionManagement(sessionManagementConfigurer ->
+                        sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
 
 
@@ -62,7 +68,6 @@ public class SecurityConfig {
         provider.setUserDetailsService(userDetailsService);
         return provider;
     }
-
 
 
 }
